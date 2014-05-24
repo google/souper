7d920879ea08cee97a19253c9c13b68dac6a8f89
Peter Collingbourne <pcc@google.com>
2014-05-23 19:40:19 -0700

    Fix use-without-inclusion error

--- unittests/Inst/InstTests.cpp
+++ unittests/Inst/InstTests.cpp
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include "llvm/Support/raw_ostream.h"
 #include "souper/Inst/Inst.h"
 
 #include "gtest/gtest.h"
