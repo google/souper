d31bc5d32b0962817256fd1cb1bd7c5984eec913
Peter Collingbourne <peter@pcc.me.uk>
2014-08-07 22:27:00 -0700

    Many parser improvements
    Specifically, type checking, type inference and support for alphanumeric
    instruction names.

--- lib/Parser/Parser.cpp
+++ lib/Parser/Parser.cpp
@@ -18,6 +18,7 @@
 #include "llvm/ADT/StringExtras.h"
 #include "llvm/ADT/StringRef.h"
 #include "llvm/ADT/StringSwitch.h"
+#include "llvm/Support/ErrorHandling.h"
 #include "llvm/Support/raw_ostream.h"
 #include "souper/Extractor/Candidates.h"
 #include "souper/Inst/Inst.h"
@@ -34,10 +35,11 @@ struct Token {
     Error,
 
     Ident,
-    InstNum,
+    ValName,
     Comma,
     Eq,
     Int,
+    UntypedInt,
     Eof,
   };
 
@@ -45,7 +47,8 @@ struct Token {
   const char *Pos;
   size_t Len;
   APInt Val;
-  unsigned Num, Width;
+  StringRef Name;
+  unsigned Width;
 
   StringRef str() const {
     return StringRef(Pos, Len);
@@ -98,34 +101,35 @@ Token Lexer::getNextToken(std::string &ErrStr) {
     }
   }
 
-  return Token{Token::Eof, Begin};
+  return Token{Token::Eof, Begin, 0, APInt()};
 
 FoundChar:
   switch (*Begin) {
     case ',':
       ++Begin;
-      return Token{Token::Comma, Begin-1, 1};
+      return Token{Token::Comma, Begin-1, 1, APInt()};
     case '=':
       ++Begin;
-      return Token{Token::Eq, Begin-1, 1};
+      return Token{Token::Eq, Begin-1, 1, APInt()};
     case '%': {
       ++Begin;
-      unsigned Num = 0;
       unsigned Width = 0;
-      const char *NumBegin = Begin;
-      while (Begin != End && *Begin >= '0' && *Begin <= '9') {
-        Num = Num*10 + (*Begin - '0');
+      const char *NameBegin = Begin;
+      while (Begin != End && ((*Begin >= '0' && *Begin <= '9') ||
+                              (*Begin >= 'A' && *Begin <= 'Z') ||
+                              (*Begin >= 'a' && *Begin <= 'z'))) {
         ++Begin;
       }
-      if (Begin == NumBegin) {
-        ErrStr = "expected integer";
-        return Token{Token::Error, Begin, 0};
+      if (Begin == NameBegin) {
+        ErrStr = "expected identifier";
+        return Token{Token::Error, Begin, 0, APInt()};
       }
+      const char *NameEnd = Begin;
       if (Begin != End && *Begin == ':') {
         ++Begin;
         if (Begin == End || *Begin != 'i') {
           ErrStr = "expected 'i'";
-          return Token{Token::Error, Begin, 0};
+          return Token{Token::Error, Begin, 0, APInt()};
         }
 
         ++Begin;
@@ -136,15 +140,19 @@ FoundChar:
         }
         if (Begin == WidthBegin) {
           ErrStr = "expected integer";
-          return Token{Token::Error, Begin, 0};
+          return Token{Token::Error, Begin, 0, APInt()};
+        }
+        if (Width == 0) {
+          ErrStr = "width must be at least 1";
+          return Token{Token::Error, WidthBegin, 0, APInt()};
         }
       }
 
       Token T;
-      T.K = Token::InstNum;
-      T.Pos = NumBegin-1;
-      T.Len = Begin - NumBegin + 1;
-      T.Num = Num;
+      T.K = Token::ValName;
+      T.Pos = NameBegin - 1;
+      T.Len = Begin - NameBegin + 1;
+      T.Name = StringRef(NameBegin, NameEnd - NameBegin);
       T.Width = Width;
       return T;
     }
@@ -155,7 +163,7 @@ FoundChar:
     do {
       ++Begin;
     } while (Begin != End && *Begin >= 'a' && *Begin <= 'z');
-    return Token{Token::Ident, TokenBegin, size_t(Begin - TokenBegin)};
+    return Token{Token::Ident, TokenBegin, size_t(Begin - TokenBegin), APInt()};
   }
 
   if (*Begin == '-' || (*Begin >= '0' && *Begin <= '9')) {
@@ -164,34 +172,39 @@ FoundChar:
       ++Begin;
     } while (Begin != End && *Begin >= '0' && *Begin <= '9');
     const char *NumEnd = Begin;
-    if (Begin == End || *Begin != ':') {
-      ErrStr = "expected ':'";
-      return Token{Token::Error, Begin, 0};
-    }
-    ++Begin;
-    if (Begin == End || *Begin != 'i') {
-      ErrStr = "expected 'i'";
-      return Token{Token::Error, Begin, 0};
-    }
-    ++Begin;
-    unsigned Width = 0;
-    const char *WidthBegin = Begin;
-    while (Begin != End && *Begin >= '0' && *Begin <= '9') {
-      Width = Width*10 + (*Begin - '0');
+    if (Begin != End && *Begin == ':') {
       ++Begin;
-    }
-    if (Begin == WidthBegin) {
-      ErrStr = "expected integer";
-      return Token{Token::Error, Begin, 0};
+      if (Begin == End || *Begin != 'i') {
+        ErrStr = "expected 'i'";
+        return Token{Token::Error, Begin, 0, APInt()};
+      }
+      ++Begin;
+      unsigned Width = 0;
+      const char *WidthBegin = Begin;
+      while (Begin != End && *Begin >= '0' && *Begin <= '9') {
+        Width = Width*10 + (*Begin - '0');
+        ++Begin;
+      }
+      if (Begin == WidthBegin) {
+        ErrStr = "expected integer";
+        return Token{Token::Error, Begin, 0, APInt()};
+      }
+      if (Width == 0) {
+        ErrStr = "width must be at least 1";
+        return Token{Token::Error, WidthBegin, 0, APInt()};
+      }
+      return Token{Token::Int, NumBegin, size_t(Begin - NumBegin),
+                   APInt(Width, StringRef(NumBegin, NumEnd - NumBegin), 10)};
     }
 
-    return Token{Token::Int, NumBegin, size_t(Begin - NumBegin),
-                 APInt(Width, StringRef(NumBegin, NumEnd - NumBegin), 10)};
+    return Token{Token::UntypedInt, NumBegin, size_t(Begin - NumBegin),
+                 APInt((NumEnd - NumBegin) * 4,
+                        StringRef(NumBegin, NumEnd - NumBegin), 10)};
   }
 
   ErrStr = std::string("unexpected '") + *Begin + "'";
   ++Begin;
-  return Token{Token::Error, Begin-1, 0};
+  return Token{Token::Error, Begin-1, 0, APInt()};
 }
 
 void souper::TestLexer(StringRef Str) {
@@ -230,8 +243,8 @@ struct Parser {
   std::vector<ParsedReplacement> &Reps;
 
   std::vector<InstMapping> PCs;
-  std::map<unsigned, Inst *> InstMap;
-  std::map<unsigned, Block *> BlockMap;
+  std::map<StringRef, Inst *> InstMap;
+  std::map<StringRef, Block *> BlockMap;
 
   std::string makeErrStr(const std::string &ErrStr) {
     return makeErrStr(L.getTokenPos(CurTok), ErrStr);
@@ -256,6 +269,13 @@ struct Parser {
   Inst *parseInst(std::string &ErrStr);
   InstMapping parseInstMapping(std::string &ErrStr);
 
+  bool typeCheckPhi(unsigned Width, Block *B, std::vector<Inst *> &Ops,
+                    std::string &ErrStr);
+  bool typeCheckInst(Inst::Kind IK, unsigned &Width, std::vector<Inst *> &Ops,
+                     std::string &ErrStr);
+  bool typeCheckOpsMatchingWidths(llvm::MutableArrayRef<Inst *> Ops,
+                                  std::string &ErrStr);
+
   bool parseLine(std::string &ErrStr);
 };
 
@@ -263,14 +283,14 @@ struct Parser {
 
 Inst *Parser::parseInst(std::string &ErrStr) {
   switch (CurTok.K) {
-    case Token::InstNum: {
+    case Token::ValName: {
       if (CurTok.Width != 0) {
         ErrStr = makeErrStr("inst reference may not have a width");
         return 0;
       }
-      auto InstBlockIt = InstMap.find(CurTok.Num);
+      auto InstBlockIt = InstMap.find(CurTok.Name);
       if (InstBlockIt == InstMap.end()) {
-        ErrStr = makeErrStr(std::string("%") + llvm::utostr(CurTok.Num) +
+        ErrStr = makeErrStr(std::string("%") + CurTok.Name.str() +
                             " is not an inst");
         return 0;
       }
@@ -286,6 +306,13 @@ Inst *Parser::parseInst(std::string &ErrStr) {
       return I;
     }
 
+    case Token::UntypedInt: {
+      Inst *I = IC.getUntypedConst(CurTok.Val);
+      if (!consumeToken(ErrStr))
+        return 0;
+      return I;
+    }
+
     default: {
       ErrStr = makeErrStr("unexpected token");
       return 0;
@@ -293,16 +320,200 @@ Inst *Parser::parseInst(std::string &ErrStr) {
   }
 }
 
+bool Parser::typeCheckOpsMatchingWidths(llvm::MutableArrayRef<Inst *> Ops,
+                                        std::string &ErrStr) {
+  unsigned Width = 0;
+  for (auto Op : Ops) {
+    if (Width == 0)
+      Width = Op->Width;
+    if (Width != 0 && Op->Width != 0 && Width != Op->Width) {
+      ErrStr = "operands have different widths";
+      return false;
+    }
+  }
+
+  if (Width == 0) {
+    ErrStr = "at least one operand must be typed";
+    return false;
+  }
+
+  for (auto &Op : Ops) {
+    if (Op->Width == 0) {
+      Op = IC.getConst(Op->Val.sextOrTrunc(Width));
+    }
+  }
+  return true;
+}
+
+bool Parser::typeCheckPhi(unsigned Width, Block *B,
+                          std::vector<Inst *> &Ops, std::string &ErrStr) {
+  if (B->Preds != Ops.size()) {
+    ErrStr = "number of operands inconsistent between phis";
+    return false;
+  }
+
+  if (!typeCheckOpsMatchingWidths(Ops, ErrStr))
+    return false;
+
+  if (Width != 0 && Width != Ops[0]->Width) {
+    ErrStr = "inst must have width of " + utostr(Ops[0]->Width) +
+                         ", has width " + utostr(Width);
+    return false;
+  }
+
+  return true;
+}
+
+bool Parser::typeCheckInst(Inst::Kind IK, unsigned &Width,
+                           std::vector<Inst *> &Ops,
+                           std::string &ErrStr) {
+  unsigned MinOps = 2, MaxOps = ~0U;
+  llvm::MutableArrayRef<Inst *> OpsMatchingWidths = Ops;
+
+  switch (IK) {
+  case Inst::Const:
+  case Inst::UntypedConst:
+  case Inst::Var:
+  case Inst::Phi:
+    llvm_unreachable("should not be handled here");
+
+  case Inst::Add:
+  case Inst::Mul:
+  case Inst::And:
+  case Inst::Or:
+  case Inst::Xor:
+    break;
+
+  case Inst::AddNSW:
+  case Inst::Sub:
+  case Inst::SubNSW:
+  case Inst::UDiv:
+  case Inst::SDiv:
+  case Inst::URem:
+  case Inst::SRem:
+  case Inst::Shl:
+  case Inst::LShr:
+  case Inst::AShr:
+  case Inst::Eq:
+  case Inst::Ne:
+  case Inst::Ult:
+  case Inst::Slt:
+  case Inst::Ule:
+  case Inst::Sle:
+    MaxOps = 2;
+    break;
+
+  case Inst::Select:
+    MinOps = MaxOps = 3;
+    if (Ops.size() > 1) {
+      if (Ops[0]->Width == 0) {
+        Ops[0] = IC.getConst(Ops[0]->Val.trunc(1));
+      } else if (Ops[0]->Width != 1) {
+        ErrStr = std::string("first operand must have width of 1, has width ") +
+                 utostr(Ops[0]->Width);
+        return false;
+      }
+      OpsMatchingWidths =
+          llvm::MutableArrayRef<Inst *>(Ops.data() + 1, Ops.size() - 1);
+    }
+    break;
+
+  case Inst::ZExt:
+  case Inst::SExt:
+    MinOps = MaxOps = 1;
+    if (Ops.size() == 1 && Width != 0 && Width <= Ops[0]->Width) {
+      ErrStr = std::string("inst must have width of at least ") +
+               utostr(Ops[0]->Width + 1) + ", has width " + utostr(Width);
+      return false;
+    }
+    break;
+
+  case Inst::Trunc:
+    MinOps = MaxOps = 1;
+    if (Ops.size() == 1 && Width != 0 && Width >= Ops[0]->Width) {
+      ErrStr = std::string("inst must have width of at most ") +
+               utostr(Ops[0]->Width - 1) + ", has width " + utostr(Width);
+      return false;
+    }
+    break;
+  }
+
+  if (MinOps == MaxOps && Ops.size() != MinOps) {
+    ErrStr = std::string("expected ") + utostr(MinOps) + " operands, found " +
+             utostr(Ops.size());
+    return false;
+  }
+
+  if (Ops.size() < MinOps) {
+    ErrStr = std::string("expected at least ") + utostr(MinOps) +
+             " operands, found " + utostr(Ops.size());
+    return false;
+  }
+
+  if (Ops.size() > MaxOps) {
+    ErrStr = std::string("expected at most ") + utostr(MaxOps) +
+             " operands, found " + utostr(Ops.size());
+    return false;
+  }
+
+  if (!typeCheckOpsMatchingWidths(OpsMatchingWidths, ErrStr))
+    return false;
+
+  unsigned ExpectedWidth;
+  switch (IK) {
+  case Inst::ZExt:
+  case Inst::SExt:
+  case Inst::Trunc:
+    ExpectedWidth = 0;
+    break;
+
+  case Inst::Select:
+    ExpectedWidth = Ops[1]->Width;
+    break;
+
+  case Inst::Eq:
+  case Inst::Ne:
+  case Inst::Ult:
+  case Inst::Slt:
+  case Inst::Ule:
+  case Inst::Sle:
+    ExpectedWidth = 1;
+    break;
+
+  default:
+    ExpectedWidth = Ops[0]->Width;
+    break;
+  }
+
+  if (Width == 0) {
+    if (ExpectedWidth == 0) {
+      ErrStr = "inst must have a width";
+      return false;
+    }
+    Width = ExpectedWidth;
+  } else if (ExpectedWidth != 0 && Width != ExpectedWidth) {
+    ErrStr = std::string("inst must have width of ") + utostr(ExpectedWidth) +
+             ", has width " + utostr(Width);
+    return false;
+  }
+
+  return true;
+}
+
 InstMapping Parser::parseInstMapping(std::string &ErrStr) {
-  Inst *S = parseInst(ErrStr);
-  if (!S)
+  Inst *SrcRep[2];
+  SrcRep[0] = parseInst(ErrStr);
+  if (!SrcRep[0])
     return InstMapping();
 
-  Inst *R = parseInst(ErrStr);
-  if (!R)
+  SrcRep[1] = parseInst(ErrStr);
+  if (!SrcRep[1])
     return InstMapping();
 
-  return InstMapping(S, R);
+  if (!typeCheckOpsMatchingWidths(SrcRep, ErrStr))
+    return InstMapping();
+
+  return InstMapping(SrcRep[0], SrcRep[1]);
 }
 
 bool Parser::parseLine(std::string &ErrStr) {
@@ -333,17 +544,17 @@ bool Parser::parseLine(std::string &ErrStr) {
         return false;
       }
 
-    case Token::InstNum: {
-      unsigned InstNum = CurTok.Num;
+    case Token::ValName: {
+      StringRef InstName = CurTok.Name;
       unsigned InstWidth = CurTok.Width;
 
-      if (InstMap.find(InstNum) != InstMap.end()) {
-        ErrStr = makeErrStr(std::string("%") + llvm::utostr(InstNum) +
+      if (InstMap.find(InstName) != InstMap.end()) {
+        ErrStr = makeErrStr(std::string("%") + InstName.str() +
                             " already declared as an inst");
         return false;
       }
-      if (BlockMap.find(InstNum) != BlockMap.end()) {
-        ErrStr = makeErrStr(std::string("%") + llvm::utostr(InstNum) +
+      if (BlockMap.find(InstName) != BlockMap.end()) {
+        ErrStr = makeErrStr(std::string("%") + InstName.str() +
                             " already declared as a block");
         return false;
       }
@@ -401,7 +612,7 @@ bool Parser::parseLine(std::string &ErrStr) {
 
           // We create the block later, as we need to know how many predecessors
           // it has.
-          BlockMap[InstNum] = 0;
+          BlockMap[InstName] = 0;
           return consumeToken(ErrStr);
         } else {
           ErrStr = makeErrStr(std::string("unexpected inst kind: '") +
@@ -410,21 +621,21 @@ bool Parser::parseLine(std::string &ErrStr) {
         }
       }
 
-      if (InstWidth == 0) {
-        ErrStr = makeErrStr(TP, "insts must have a width");
+      if (IK == Inst::Var && InstWidth == 0) {
+        ErrStr = makeErrStr(TP, "var must have a width");
         return false;
       }
 
       if (!consumeToken(ErrStr)) return false;
 
-      std::map<unsigned, Block *>::iterator PhiBlockIt;
+      std::map<StringRef, Block *>::iterator PhiBlockIt;
       if (IK == Inst::Var) {
-        InstMap[InstNum] = IC.createVar(InstWidth, "");
+        InstMap[InstName] = IC.createVar(InstWidth, InstName);
         return true;
       }
 
       if (IK == Inst::Phi) {
-        if (CurTok.K != Token::InstNum) {
+        if (CurTok.K != Token::ValName) {
           ErrStr = makeErrStr("expected block number");
           return false;
         }
@@ -432,10 +643,10 @@ bool Parser::parseLine(std::string &ErrStr) {
           ErrStr = makeErrStr("blocks may not have a width");
           return false;
         }
-        PhiBlockIt = BlockMap.find(CurTok.Num);
+        PhiBlockIt = BlockMap.find(CurTok.Name);
         if (PhiBlockIt == BlockMap.end()) {
-          ErrStr = makeErrStr(std::string("%") + llvm::utostr(InstNum) +
-                              " is not a block");
+          ErrStr =
+              makeErrStr(std::string("%") + InstName.str() + " is not a block");
           return false;
         }
         if (!consumeToken(ErrStr)) return false;
@@ -465,12 +676,20 @@ bool Parser::parseLine(std::string &ErrStr) {
         if (!PhiBlockIt->second) {
           PhiBlockIt->second = IC.createBlock(Ops.size());
         }
+        if (!typeCheckPhi(InstWidth, PhiBlockIt->second, Ops, ErrStr)) {
+          ErrStr = makeErrStr(TP, ErrStr);
+          return false;
+        }
         I = IC.getPhi(PhiBlockIt->second, Ops);
       } else {
+        if (!typeCheckInst(IK, InstWidth, Ops, ErrStr)) {
+          ErrStr = makeErrStr(TP, ErrStr);
+          return false;
+        }
         I = IC.getInst(IK, InstWidth, Ops);
       }
 
-      InstMap[InstNum] = I;
+      InstMap[InstName] = I;
       return true;
     }
 
--- unittests/Parser/ParserTests.cpp
+++ unittests/Parser/ParserTests.cpp
@@ -22,37 +22,98 @@ TEST(ParserTest, Errors) {
   struct {
     std::string Test, WantError;
   } Tests[] = {
-        {"%foo", "<input>:1:2: expected integer"},
-        {"%0:j", "<input>:1:4: expected 'i'"},
-        {"%0:ix", "<input>:1:5: expected integer"},
-        {"0?", "<input>:1:2: expected ':'"},
-        {"0:j", "<input>:1:3: expected 'i'"},
-        {"0:ix", "<input>:1:4: expected integer"},
-        {"?", "<input>:1:1: unexpected '?'"},
-        {"cand 0:i1 0:i1 ; this is a comment\n", ""},
+      // lexing
+      { "%=", "<input>:1:2: expected identifier" },
+      { "%0:j", "<input>:1:4: expected 'i'" },
+      { "%0:ix", "<input>:1:5: expected integer" },
+      { "%0:i0", "<input>:1:5: width must be at least 1" },
+      { "0:j", "<input>:1:3: expected 'i'" },
+      { "0:ix", "<input>:1:4: expected integer" },
+      { "0:i0", "<input>:1:4: width must be at least 1" },
+      { "?", "<input>:1:1: unexpected '?'" },
+      { "cand 0:i1 0:i1 ; this is a comment\n", "" },
 
-        {"%0:i32 = var\n%1:i1 = eq %0:i32, %0:i32\n",
-         "<input>:2:12: inst reference may not have a width"},
-        {"%0:i1 = eq %1, %1\n", "<input>:1:12: %1 is not an inst"},
-        {"%0:i1 = eq foo\n", "<input>:1:12: unexpected token"},
-        {"%0:i1 = var\n%0:i1 = var\n",
-         "<input>:2:1: %0 already declared as an inst"},
-        {"%0 = block\n%0:i1 = var\n",
-         "<input>:2:1: %0 already declared as a block"},
-        {"%0 foo\n", "<input>:1:4: expected '='"},
-        {"%0 = %1\n", "<input>:1:6: expected identifier"},
-        {"%0:i1 = block\n", "<input>:1:1: blocks may not have a width"},
-        {"%0:i1 = foo\n", "<input>:1:9: unexpected inst kind: 'foo'"},
-        {"%0 = var\n", "<input>:1:1: insts must have a width"},
-        {"%0:i1 = phi foo\n", "<input>:1:13: expected block number"},
-        {"%0:i1 = block\n", "<input>:1:1: blocks may not have a width"},
-        {"%0:i1 = phi %0\n", "<input>:1:13: %0 is not a block"},
-        {"%0 = block\n%1:i1 = phi %0 foo\n", "<input>:2:16: expected ','"},
-        {",\n", "<input>:1:1: expected inst, block, cand or pc"},
-        {"cand 0:i1 0:i1\ncand 0:i1 0:i1\n",
-         "<input>:2:1: expected a single replacement"},
-        {"%0:i1 = var\n",
-         "<input>:2:1: incomplete replacement, need a 'cand' statement"},
+      // parsing
+      { "%0:i32 = var\n%1:i1 = eq %0:i32, %0:i32\n",
+        "<input>:2:12: inst reference may not have a width" },
+      { "%0:i1 = eq %1, %1\n", "<input>:1:12: %1 is not an inst" },
+      { "%0:i1 = eq foo\n", "<input>:1:12: unexpected token" },
+      { "%0:i1 = var\n%0:i1 = var\n",
+        "<input>:2:1: %0 already declared as an inst" },
+      { "%0 = block\n%0:i1 = var\n",
+        "<input>:2:1: %0 already declared as a block" },
+      { "%0 foo\n", "<input>:1:4: expected '='" },
+      { "%0 = %1\n", "<input>:1:6: expected identifier" },
+      { "%0:i1 = block\n", "<input>:1:1: blocks may not have a width" },
+      { "%0:i1 = foo\n", "<input>:1:9: unexpected inst kind: 'foo'" },
+      { "%0 = var\n", "<input>:1:1: var must have a width" },
+      { "%0:i1 = phi foo\n", "<input>:1:13: expected block number" },
+      { "%0:i1 = block\n", "<input>:1:1: blocks may not have a width" },
+      { "%0:i1 = phi %0\n", "<input>:1:13: %0 is not a block" },
+      { "%0 = block\n%1:i1 = phi %0 foo\n", "<input>:2:16: expected ','" },
+      { ",\n", "<input>:1:1: expected inst, block, cand or pc" },
+      { "cand 0:i1 0:i1\ncand 0:i1 0:i1\n",
+        "<input>:2:1: expected a single replacement" },
+      { "%0:i1 = var\n",
+        "<input>:2:1: incomplete replacement, need a 'cand' statement" },
+
+      // type checking
+      { "%0 = add 1:i32\n",
+        "<input>:1:1: expected at least 2 operands, found 1" },
+      { "%0:i64 = add 1:i32, 2:i32\n",
+        "<input>:1:1: inst must have width of 32, has width 64" },
+      { "%0 = add 1:i32, 2:i64\n",
+        "<input>:1:1: operands have different widths" },
+
+      { "%0 = addnsw 1:i32\n",
+        "<input>:1:1: expected 2 operands, found 1" },
+      { "%0 = addnsw 1:i32, 2:i32, 3:i32\n",
+        "<input>:1:1: expected 2 operands, found 3" },
+      { "%0:i64 = addnsw 1:i32, 2:i32\n",
+        "<input>:1:1: inst must have width of 32, has width 64" },
+      { "%0 = addnsw 1:i32, 2:i64\n",
+        "<input>:1:1: operands have different widths" },
+
+      { "%0 = eq 1:i32\n",
+        "<input>:1:1: expected 2 operands, found 1" },
+      { "%0 = eq 1:i32, 2:i32, 3:i32\n",
+        "<input>:1:1: expected 2 operands, found 3" },
+      { "%0:i64 = eq 1:i32, 2:i32\n",
+        "<input>:1:1: inst must have width of 1, has width 64" },
+      { "%0 = eq 1:i32, 2:i64\n",
+        "<input>:1:1: operands have different widths" },
+
+      { "%0 = select 1:i1, 2:i32\n",
+        "<input>:1:1: expected 3 operands, found 2" },
+      { "%0 = select 1:i1, 2:i32, 3:i32, 4:i32\n",
+        "<input>:1:1: expected 3 operands, found 4" },
+      { "%0 = select 1:i32, 2:i32, 3:i32\n",
+        "<input>:1:1: first operand must have width of 1, has width 32" },
+      { "%0:i64 = select 1:i1, 2:i32, 3:i32\n",
+        "<input>:1:1: inst must have width of 32, has width 64" },
+      { "%0 = select 1:i1, 2:i32, 3:i64\n",
+        "<input>:1:1: operands have different widths" },
+
+      { "%0 = zext 1:i1\n",
+        "<input>:1:1: inst must have a width" },
+      { "%0:i33 = zext 1:i1, 2:i32\n",
+        "<input>:1:1: expected 1 operands, found 2" },
+      { "%0:i32 = zext 1:i64\n",
+        "<input>:1:1: inst must have width of at least 65, has width 32" },
+
+      { "%0 = trunc 1:i1\n",
+        "<input>:1:1: inst must have a width" },
+      { "%0:i33 = trunc 1:i1, 2:i32\n",
+        "<input>:1:1: expected 1 operands, found 2" },
+      { "%0:i64 = trunc 1:i32\n",
+        "<input>:1:1: inst must have width of at most 31, has width 64" },
+
+      { "%0 = block\n%1 = phi %0, 1:i32, 2:i32\n%2 = phi %0, 3:i32\n",
+        "<input>:3:1: number of operands inconsistent between phis" },
+      { "%0 = block\n%1 = phi %0, 1:i32, 2:i64\n",
+        "<input>:2:1: operands have different widths" },
+      { "%0 = block\n%1:i32 = phi %0, 1:i64, 2:i64\n",
+        "<input>:2:1: inst must have width of 64, has width 32" },
     };
 
   InstContext IC;
@@ -67,9 +128,9 @@ TEST(ParserTest, RoundTrip) {
   std::string Tests[] = {
       "cand 0:i1 0:i1\n",
       R"i(%0 = block
-%1:i32 = var
+%1:i32 = var ; 1
 %2:i32 = lshr %1, 31:i32
-%3:i32 = var
+%3:i32 = var ; 3
 %4:i32 = udiv %2, %3
 %5:i1 = eq 0:i32, %3
 %6:i32 = zext %5
@@ -80,15 +141,49 @@ cand %9 1:i1
 )i",
   };
 
+  struct {
+    std::string Test, Want;
+  } NonEqualTests[] = {
+    {R"i(%foo = add 1, 2:i32
+cand %foo 3:i32
+)i",
+     R"i(%0:i32 = add 1:i32, 2:i32
+cand %0 3:i32
+)i"},
+    {R"i(%foo = add 1:i32, 2
+cand %foo 3
+)i",
+     R"i(%0:i32 = add 1:i32, 2:i32
+cand %0 3:i32
+)i"},
+    {R"i(%foo = select 1, 2:i32, 3
+cand %foo 3
+)i",
+     R"i(%0:i32 = select 1:i1, 2:i32, 3:i32
+cand %0 3:i32
+)i"},
+  };
+
   InstContext IC;
   for (const auto &T : Tests) {
     std::string ErrStr;
     auto R = ParseReplacement(IC, "<input>", T, ErrStr);
-    EXPECT_EQ("", ErrStr);
+    ASSERT_EQ("", ErrStr);
 
     std::string NewStr;
     llvm::raw_string_ostream OS(NewStr);
     R.print(OS);
-    EXPECT_EQ(T, OS.str());
+    EXPECT_EQ(OS.str(), T);
+  }
+
+  for (const auto &T : NonEqualTests) {
+    std::string ErrStr;
+    auto R = ParseReplacement(IC, "<input>", T.Test, ErrStr);
+    ASSERT_EQ("", ErrStr);
+
+    std::string NewStr;
+    llvm::raw_string_ostream OS(NewStr);
+    R.print(OS);
+    EXPECT_EQ(OS.str(), T.Want);
   }
 }
