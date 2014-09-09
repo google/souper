9f37840ed0246448b88f7b1860757c29a1dc473d
John Regehr <regehr@cs.utah.edu>
2014-09-09 15:48:12 -0600

    make sure sclang doesn't invoke souper in a mode that expects redis to be running

--- test/Pass/clang-pass-profile.c
+++ test/Pass/clang-pass-profile.c
@@ -1,6 +1,6 @@
 // REQUIRES: solver
 
-// RUN: env SOUPER_PROFILE=1 env SCLANG_SOLVER=%solver %sclang -O %s -o %t
+// RUN: env SCLANG_NO_EXTERNAL_CACHE=1 env SOUPER_PROFILE=1 env SCLANG_SOLVER=%solver %sclang -O %s -o %t
 
 // RUN: env SOUPER_PROFILE_TO_STDOUT=1 %t 0 | FileCheck %s -check-prefix=ARG0
 // ARG0: count = 0
