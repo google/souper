3644f952f37977981324970125dfa6af6565fb65
John Regehr <regehr@cs.utah.edu>
2018-01-06 15:36:24 -0700

    llvm 6

--- update_deps.sh
+++ update_deps.sh
@@ -15,7 +15,7 @@
 # limitations under the License.
 
 hiredis_commit=8f60ee65327445ed8384290b4040685329eb03c5
-llvm_branch=branches/release_50
+llvm_branch=branches/release_60
 klee_repo=https://github.com/rsas/klee
 klee_branch=pure-bv-qf-llvm-5.0-patch
 
