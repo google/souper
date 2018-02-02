d0b86baee14b095e67e764ef1b8c6cb16d533d5a
Zhengyang Liu <liuz@cs.utah.edu>
2018-02-02 16:48:39 -0700

    Added prefix "%" to clang command for C related test cases

--- test/Extractor/pr296.c
+++ test/Extractor/pr296.c
@@ -1,6 +1,6 @@
 // REQUIRES: solver
 
-// RUN: clang++ -Xclang -load -Xclang %pass -O2 -std=c++11 -mllvm %solver -emit-llvm -S -o - %s
+// RUN: %clang++ -Xclang -load -Xclang %pass -O2 -std=c++11 -mllvm %solver -emit-llvm -S -o - %s
 
 // Regression test for pull request #296
 void *a;
