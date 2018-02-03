26a9ab5f8c9b3061d0429462681856caa589ea9a
John Regehr <regehr@cs.utah.edu>
2018-02-02 19:17:37 -0700

    I noticed that our testing of the opt pass and sclang is quite weak. These tests start to address that. The trick is to write code that is robustly optimized by souper and robustly not-optimized by LLVM. Hopefully these accomplisht that.

---
+++ test/Pass/sclang-prime.cpp
@@ -0,0 +1,22 @@
+// REQUIRES: solver
+// RUN: SOUPER_SOLVER=%solver SOUPER_NO_EXTERNAL_CACHE=1 %sclang++ -O %s -S -o - -emit-llvm 2>&1 | FileCheck %s
+// CHECK: ret i1 false
+
+#include <limits.h>
+
+static bool divides_evenly(unsigned short prime, unsigned short n) {
+  return (n <= 1 || n >= prime) ? false : (prime % n) == 0;
+}
+
+bool always_false(void) {
+  for (int n = 0; n <= USHRT_MAX; ++n) {
+    if (divides_evenly(29, n) ||
+        divides_evenly(71, n) ||
+        divides_evenly(113, n) ||
+        divides_evenly(65267, n) ||
+        divides_evenly(65381, n) ||
+        divides_evenly(65497, n))
+      return true;
+  }
+  return false;
+}
