0974a75a6a5c8c5aa9bf3d0270055a4bc876480a
John Regehr <regehr@cs.utah.edu>
2019-03-02 16:53:58 -0700

    add poison and UB support to the interpreter

--- lib/Infer/Interpreter.cpp
+++ lib/Infer/Interpreter.cpp
@@ -1,135 +1,364 @@
 #include "souper/Infer/Interpreter.h"
 
 namespace souper {
-namespace {
-EvalValue getValue(Inst *I, ValueCache &C) {
-  if (I->K == souper::Inst::Const) {
-      return {I->Val};
-  } else if (I->K == souper::Inst::Var
-      || I->K == souper::Inst::ReservedConst
-      || I->K == souper::Inst::ReservedInst) {
-    if (I->Name != "" && C.find(I) != C.end()) {
-      return C[I];
-    } else {
-      return EvalValue(); // unimplemented
-    }
-  }
-  return EvalValue();
-}
-}
 
+#define ARG0 Args[0].getValue()
+#define ARG1 Args[1].getValue()
+#define ARG2 Args[2].getValue()
 
-EvalValue evaluateSingleInst(souper::Inst* Inst,
-                             std::vector<EvalValue> args) {
-  switch (Inst->K) {
-    case souper::Inst::Const:
-      return {Inst->Val};
-    case souper::Inst::UntypedConst:
-      return {Inst->Val};
-    case souper::Inst::Var:
-      llvm_unreachable("Should get value from cache without reaching here");
-    case souper::Inst::Add:
-      return {args[0].getValue() + args[1].getValue()};
-    case souper::Inst::Sub:
-      return {args[0].getValue() - args[1].getValue()};
-    case souper::Inst::Mul:
-      return {args[0].getValue() * args[1].getValue()};
-    case souper::Inst::UDiv:
-      if (args[1].getValue() == 0) {
-        return EvalValue::ub();
-      }
-      return {args[0].getValue().udiv(args[1].getValue())};
-    case souper::Inst::SDiv:
-      if (args[1].getValue() == 0) {
-        return EvalValue::ub();
-      }
-      return {args[0].getValue().sdiv(args[1].getValue())};
-    case souper::Inst::URem:
-      return {args[0].getValue().urem(args[1].getValue())};
-    case souper::Inst::SRem:
-      return {args[0].getValue().srem(args[1].getValue())};
-    case souper::Inst::And:
-      return {args[0].getValue() & args[1].getValue()};
-    case souper::Inst::Or:
-      return {args[0].getValue() | args[1].getValue()};
-    case souper::Inst::Xor:
-      return {args[0].getValue() ^ args[1].getValue()};
-    case souper::Inst::Shl:
-      return {args[0].getValue() << args[1].getValue()};
-    case souper::Inst::LShr:
-      return {args[0].getValue().lshr(args[1].getValue())};
-    case souper::Inst::AShr:
-      return {args[0].getValue().ashr(args[1].getValue())};
-    // TODO: Handle NSW/NUW/NW variants of instructions
-    case souper::Inst::Select: {
-      if (!args[0].hasValue()) {
-        return args[0];
-      } else {
-        bool cond = args[0].getValue().getBoolValue();
-        return cond ? args[1] : args[2];
-      }
+  EvalValue evaluateSingleInst(Inst *Inst, std::vector<EvalValue> &Args) {
+
+    // UB propagates unconditionally
+    for (auto &A : Args)
+      if (A.K == EvalValue::ValueKind::UB)
+	return EvalValue::ub();
+
+    // phi and select only take poison from their chosen input
+    if (Inst->K != Inst::Phi && Inst->K != Inst::Select) {
+      for (auto &A : Args)
+	if (A.K == EvalValue::ValueKind::Poison)
+	  return EvalValue::poison();
     }
-    case souper::Inst::ZExt:
-      return {args[0].getValue().zext(Inst->Width)};
-    case souper::Inst::SExt:
-      return {args[0].getValue().sext(Inst->Width)};
-    case souper::Inst::Trunc:
-      return {args[0].getValue().trunc(Inst->Width)};
-    case souper::Inst::Eq:
-      return {{1, args[0].getValue() == args[1].getValue()}};
-    case souper::Inst::Ne:
-      return {{1, args[0].getValue() != args[1].getValue()}};
-    case souper::Inst::Ult:
-      return {{1, args[0].getValue().ult(args[1].getValue())}};
-    case souper::Inst::Slt:
-      return {{1, args[0].getValue().slt(args[1].getValue())}};
-    case souper::Inst::Ule:
-      return {{1, args[0].getValue().ule(args[1].getValue())}};
-    case souper::Inst::Sle:
-      return {{1, args[0].getValue().sle(args[1].getValue())}};
-    case souper::Inst::CtPop:
-      return {llvm::APInt(Inst->Width, args[0].getValue().countPopulation())};
-    case souper::Inst::Ctlz:
+
+    for (auto &A : Args)
+      if (A.K == EvalValue::ValueKind::Undef)
+	llvm::report_fatal_error("undef not supported by interpreter");
+
+    switch (Inst->K) {
+    case Inst::Const:
+      return {Inst->Val};
+
+    case Inst::UntypedConst:
+      return {Inst->Val};
+
+    case Inst::Var:
+      llvm::report_fatal_error("Interpreter can't find an input value, exiting");
+
+    case Inst::Phi:
+      // FIXME should return all values not just one of them
+      return ARG0;
+
+    case Inst::Add:
+      return {ARG0 + ARG1};
+
+    case Inst::AddNSW:{
+      bool Ov;
+      auto Res = ARG0.sadd_ov(ARG1, Ov);
+      if (Ov)
+        return EvalValue::poison();
+      else
+        return Res;
+    }
+
+    case Inst::AddNUW:{
+      bool Ov;
+      auto Res = ARG0.uadd_ov(ARG1, Ov);
+      if (Ov)
+        return EvalValue::poison();
+      else
+        return Res;
+    }
+
+    case Inst::AddNW:{
+      bool Ov1, Ov2;
+      auto Res1 = ARG0.sadd_ov(ARG1, Ov1);
+      auto Res2 = ARG0.uadd_ov(ARG1, Ov2);
+      if (Ov1 || Ov2)
+        return EvalValue::poison();
+      else
+        return Res1;
+    }
+
+    case Inst::Sub:
+      return {ARG0 - ARG1};
+
+    case Inst::SubNSW:{
+      bool Ov;
+      auto Res = ARG0.ssub_ov(ARG1, Ov);
+      if (Ov)
+        return EvalValue::poison();
+      else
+        return Res;
+    }
+
+    case Inst::SubNUW:{
+      bool Ov;
+      auto Res = ARG0.usub_ov(ARG1, Ov);
+      if (Ov)
+        return EvalValue::poison();
+      else
+        return Res;
+    }
+
+    case Inst::SubNW:{
+      bool Ov1, Ov2;
+      auto Res1 = ARG0.ssub_ov(ARG1, Ov1);
+      auto Res2 = ARG0.usub_ov(ARG1, Ov2);
+      if (Ov1 || Ov2)
+        return EvalValue::poison();
+      else
+        return Res1;
+    }
+
+    case Inst::Mul:
+      return {ARG0 * ARG1};
+
+    case Inst::MulNSW:{
+      bool Ov;
+      auto Res = ARG0.smul_ov(ARG1, Ov);
+      if (Ov)
+        return EvalValue::poison();
+      else
+        return Res;
+    }
+
+    case Inst::MulNUW:{
+      bool Ov;
+      auto Res = ARG0.umul_ov(ARG1, Ov);
+      if (Ov)
+        return EvalValue::poison();
+      else
+        return Res;
+    }
+
+    case Inst::MulNW:{
+      bool Ov1, Ov2;
+      auto Res1 = ARG0.smul_ov(ARG1, Ov1);
+      auto Res2 = ARG0.umul_ov(ARG1, Ov2);
+      if (Ov1 || Ov2)
+        return EvalValue::poison();
+      else
+        return Res1;
+    }
+
+    case Inst::UDivExact:
+      if (ARG1 == 0)
+        return EvalValue::ub();
+      if ((ARG0.udiv(ARG1) * ARG1) != ARG0)
+        return EvalValue::poison();
+      return {ARG0.udiv(ARG1)};
+
+    case Inst::UDiv:
+      if (ARG1 == 0)
+        return EvalValue::ub();
+      return {ARG0.udiv(ARG1)};
+
+    case Inst::SDivExact:
+      if (ARG1 == 0 ||
+          (ARG0.isMinSignedValue() && ARG1.isAllOnesValue()))
+        return EvalValue::ub();
+      if ((ARG0.sdiv(ARG1) * ARG1) != ARG0)
+        return EvalValue::poison();
+      return {ARG0.sdiv(ARG1)};
+
+    case Inst::SDiv:
+      if (ARG1 == 0 ||
+          (ARG0.isMinSignedValue() && ARG1 == -1))
+        return EvalValue::ub();
+      return {ARG0.sdiv(ARG1)};
+
+    case Inst::URem:
+      if (ARG1 == 0)
+        return EvalValue::ub();
+      return {ARG0.urem(ARG1)};
+
+    case Inst::SRem:
+      if (ARG1 == 0 ||
+          (ARG0.isMinSignedValue() && ARG1.isAllOnesValue()))
+        return EvalValue::ub();
+      return {ARG0.srem(ARG1)};
+
+    case Inst::And:
+      return {ARG0 & ARG1};
+
+    case Inst::Or:
+      return {ARG0 | ARG1};
+
+    case Inst::Xor:
+      return {ARG0 ^ ARG1};
+
+    case Inst::Shl:
+      if (ARG1.uge(ARG0.getBitWidth()))
+	return EvalValue::poison();
+      return {ARG0 << ARG1};
+
+    case Inst::ShlNSW:{
+      bool Ov;
+      auto Res = ARG0.sshl_ov(ARG1, Ov);
+      if (Ov)
+	return EvalValue::poison();
+      return Res;
+    }
+
+    case Inst::ShlNUW:{
+      bool Ov;
+      auto Res = ARG0.ushl_ov(ARG1, Ov);
+      if (Ov)
+	return EvalValue::poison();
+      return Res;
+    }
+
+    case Inst::ShlNW:{
+      bool Ov1, Ov2;
+      auto Res1 = ARG0.ushl_ov(ARG1, Ov1);
+      auto Res2 = ARG0.sshl_ov(ARG1, Ov2);
+      if (Ov1 || Ov2)
+	return EvalValue::poison();
+      return Res1;
+    }
+
+    case Inst::LShr:
+      if (ARG1.uge(ARG0.getBitWidth()))
+	return EvalValue::poison();
+      return {ARG0.lshr(ARG1)};
+
+    case Inst::LShrExact:{
+      if (ARG1.uge(ARG0.getBitWidth()))
+	return EvalValue::poison();
+      auto Res = ARG0.lshr(ARG1);
+      if (ARG0 != Res.shl(ARG1))
+	return EvalValue::poison();
+      return Res;
+    }
+
+    case Inst::AShr:
+      if (ARG1.uge(ARG0.getBitWidth()))
+	return EvalValue::poison();
+      return {ARG0.ashr(ARG1)};
+
+    case Inst::AShrExact:{
+      if (ARG1.uge(ARG0.getBitWidth()))
+	return EvalValue::poison();
+      auto Res = ARG0.ashr(ARG1);
+      if (ARG0 != Res.shl(ARG1))
+	return EvalValue::poison();
+      return Res;
+    }
+
+    case Inst::Select:
+      if (!Args[0].hasValue())
+        return EvalValue::ub();
+      return ARG0.getBoolValue() ? Args[1] : Args[2];
+
+    case Inst::ZExt:
+      return {ARG0.zext(Inst->Width)};
+
+    case Inst::SExt:
+      return {ARG0.sext(Inst->Width)};
+
+    case Inst::Trunc:
+      return {ARG0.trunc(Inst->Width)};
+
+    case Inst::Eq:
+      return {{1, ARG0 == ARG1}};
+
+    case Inst::Ne:
+      return {{1, ARG0 != ARG1}};
+
+    case Inst::Ult:
+      return {{1, ARG0.ult(ARG1)}};
+
+    case Inst::Slt:
+      return {{1, ARG0.slt(ARG1)}};
+
+    case Inst::Ule:
+      return {{1, ARG0.ule(ARG1)}};
+
+    case Inst::Sle:
+      return {{1, ARG0.sle(ARG1)}};
+
+    case Inst::CtPop:
+      return {llvm::APInt(Inst->Width, ARG0.countPopulation())};
+
+    case Inst::Ctlz:
       return {llvm::APInt(Inst->Width,
-                          args[0].getValue().countLeadingZeros())};
-    case souper::Inst::Cttz:
+                          ARG0.countLeadingZeros())};
+
+    case Inst::Cttz:
       return {llvm::APInt(Inst->Width,
-                          args[0].getValue().countTrailingZeros())};
-    case souper::Inst::BSwap:
-      return {args[0].getValue().byteSwap()};
+                          ARG0.countTrailingZeros())};
+
+    case Inst::BSwap:
+      return {ARG0.byteSwap()};
+
+    case Inst::BitReverse:
+      return {ARG0.reverseBits()};
+
+    case Inst::FShl:{
+      unsigned W = ARG0.getBitWidth();
+      auto Input = (ARG0.zext(2 * W).shl(W)) | ARG1.zext(2 * W);
+      auto Output = Input.shl(ARG2.urem(W));
+      return {Output.extractBits(W, W)};
+    }
+
+    case Inst::FShr:{
+      unsigned W = ARG0.getBitWidth();
+      auto Input = (ARG0.zext(2 * W).shl(W)) | ARG1.zext(2 * W);
+      auto Output = Input.lshr(ARG2.urem(W));
+      return {Output.extractBits(W, 0)};
+    }
+
+    // TODO implement these
+    case Inst::ExtractValue:
+    case Inst::SAddWithOverflow:
+    case Inst::SAddO:
+    case Inst::UAddWithOverflow:
+    case Inst::UAddO:
+    case Inst::SSubWithOverflow:
+    case Inst::SSubO:
+    case Inst::USubWithOverflow:
+    case Inst::USubO:
+    case Inst::SMulWithOverflow:
+    case Inst::SMulO:
+    case Inst::UMulWithOverflow:
+    case Inst::UMulO:
+      return {EvalValue()};
+
     default:
-      return EvalValue(); // unimplemented
-  }
-}
-
-// Errs on the side of an 'unimplemented' result
-// TODO: Some instructions unimplemented
-EvalValue evaluateInst(souper::Inst* Root, ValueCache &Cache) {
-  std::vector<EvalValue> EvaluatedArgs;
-  EvalValue Result;
-
-  if (Root->K == souper::Inst::Var) {
-      Result = Cache[Root];
-  } else {
-    for (auto &&I : Root->Ops) {
-      auto ArgEval = evaluateInst(I, Cache);
-      if (!ArgEval.hasValue() && Root->K != souper::Inst::Select) {
-        return ArgEval;
-        // result has the same ValueKind as the argument which
-        // failed to produce a ValueKind::Val
-      }
-      EvaluatedArgs.push_back(ArgEval);
+      llvm::report_fatal_error("unimplemented instruction kind " +
+                               std::string(Inst::getKindName(Inst->K)) +
+                               " in interpreter");
     }
-    Result = evaluateSingleInst(Root, EvaluatedArgs);
   }
-  return Result;
-}
 
-llvm::KnownBits findKnownBits(Inst* I, ValueCache& C) {
-  llvm::KnownBits Result(I->Width);
-  switch(I->K) {
-    case souper::Inst::Const:
-    case souper::Inst::Var : {
+#undef ARG0
+#undef ARG1
+#undef ARG2
+
+  EvalValue evaluateInst(Inst *Root, ValueCache &Cache) {
+    // TODO populate cache and look things up in it
+    // needed?
+    if (Root->K == Inst::Var) {
+      return Cache[Root];
+    } else {
+      // TODO SmallVector
+      std::vector<EvalValue> EvaluatedArgs;
+      for (auto &&I : Root->Ops)
+        EvaluatedArgs.push_back(evaluateInst(I, Cache));
+      auto Result = evaluateSingleInst(Root, EvaluatedArgs);
+      //Cache[Root] = Result;
+      return Result;
+    }
+  }
+
+  EvalValue getValue(Inst *I, ValueCache &C) {
+    if (I->K == Inst::Const)
+      return {I->Val};
+    if (I->K == Inst::Var ||
+	I->K == Inst::ReservedConst ||
+	I->K == Inst::ReservedInst) {
+      if (C.find(I) != C.end())
+	return C[I];
+    }
+    // unimplemented
+    return EvalValue();
+  }
+
+  llvm::KnownBits findKnownBits(Inst *I, ValueCache &C) {
+    llvm::KnownBits Result(I->Width);
+    switch(I->K) {
+    case Inst::Const:
+    case Inst::Var : {
       EvalValue V = getValue(I, C);
       if (V.hasValue()) {
         Result.One = V.getValue();
@@ -139,7 +368,7 @@ llvm::KnownBits findKnownBits(Inst* I, ValueCache& C) {
         return Result;
       }
     }
-    case souper::Inst::Shl : {
+    case Inst::Shl : {
       auto Op0KB = findKnownBits(I->Ops[0], C);
       auto Op1V = getValue(I->Ops[1], C);
       if (Op1V.hasValue()) {
@@ -152,7 +381,7 @@ llvm::KnownBits findKnownBits(Inst* I, ValueCache& C) {
         return Result;
       }
     }
-    case souper::Inst::And : {
+    case Inst::And : {
       auto Op0KB = findKnownBits(I->Ops[0], C);
       auto Op1KB = findKnownBits(I->Ops[1], C);
 
@@ -160,7 +389,7 @@ llvm::KnownBits findKnownBits(Inst* I, ValueCache& C) {
       Op0KB.Zero |= Op1KB.Zero;
       return Op0KB;
     }
-    case souper::Inst::Or : {
+    case Inst::Or : {
       auto Op0KB = findKnownBits(I->Ops[0], C);
       auto Op1KB = findKnownBits(I->Ops[1], C);
 
@@ -168,7 +397,7 @@ llvm::KnownBits findKnownBits(Inst* I, ValueCache& C) {
       Op0KB.Zero &= Op1KB.Zero;
       return Op0KB;
     }
-    case souper::Inst::Xor : {
+    case Inst::Xor : {
       auto Op0KB = findKnownBits(I->Ops[0], C);
       auto Op1KB = findKnownBits(I->Ops[1], C);
       llvm::APInt KnownZeroOut =
@@ -180,15 +409,15 @@ llvm::KnownBits findKnownBits(Inst* I, ValueCache& C) {
     }
     default :
       return Result;
+    }
   }
-}
 
-llvm::ConstantRange findConstantRange(souper::Inst* I,
-                                      souper::ValueCache& C) {
-  llvm::ConstantRange result(I->Width);
-  switch (I->K) {
-    case souper::Inst::Const:
-    case souper::Inst::Var : {
+  llvm::ConstantRange findConstantRange(Inst *I,
+                                        ValueCache &C) {
+    llvm::ConstantRange result(I->Width);
+    switch (I->K) {
+    case Inst::Const:
+    case Inst::Var : {
       EvalValue V = getValue(I, C);
       if (V.hasValue()) {
         return llvm::ConstantRange(V.getValue());
@@ -196,24 +425,24 @@ llvm::ConstantRange findConstantRange(souper::Inst* I,
         return result; // Whole range
       }
     }
-    case souper::Inst::Trunc: {
+    case Inst::Trunc: {
       auto R0 = findConstantRange(I->Ops[0], C);
       return R0.truncate(I->Width);
     }
-    case souper::Inst::SExt: {
+    case Inst::SExt: {
       auto R0 = findConstantRange(I->Ops[0], C);
       return R0.signExtend(I->Width);
     }
-    case souper::Inst::ZExt: {
+    case Inst::ZExt: {
       auto R0 = findConstantRange(I->Ops[0], C);
       return R0.zeroExtend(I->Width);
     }
-    case souper::Inst::Add: {
+    case Inst::Add: {
       auto R0 = findConstantRange(I->Ops[0], C);
       auto R1 = findConstantRange(I->Ops[1], C);
       return R0.add(R1);
     }
-    case souper::Inst::AddNSW: {
+    case Inst::AddNSW: {
       auto R0 = findConstantRange(I->Ops[0], C);
       auto V1 = getValue(I->Ops[1], C);
       if (V1.hasValue()) {
@@ -222,55 +451,55 @@ llvm::ConstantRange findConstantRange(souper::Inst* I,
         return result; // full range, can we do better?
       }
     }
-    case souper::Inst::Sub: {
+    case Inst::Sub: {
       auto R0 = findConstantRange(I->Ops[0], C);
       auto R1 = findConstantRange(I->Ops[1], C);
       return R0.sub(R1);
     }
-    case souper::Inst::Mul: {
+    case Inst::Mul: {
       auto R0 = findConstantRange(I->Ops[0], C);
       auto R1 = findConstantRange(I->Ops[1], C);
       return R0.multiply(R1);
     }
-    case souper::Inst::And: {
+    case Inst::And: {
       auto R0 = findConstantRange(I->Ops[0], C);
       auto R1 = findConstantRange(I->Ops[1], C);
       return R0.binaryAnd(R1);
     }
-    case souper::Inst::Or: {
+    case Inst::Or: {
       auto R0 = findConstantRange(I->Ops[0], C);
       auto R1 = findConstantRange(I->Ops[1], C);
       return R0.binaryOr(R1);
     }
-    case souper::Inst::Shl: {
+    case Inst::Shl: {
       auto R0 = findConstantRange(I->Ops[0], C);
       auto R1 = findConstantRange(I->Ops[1], C);
       return R0.shl(R1);
     }
-    case souper::Inst::AShr: {
+    case Inst::AShr: {
       auto R0 = findConstantRange(I->Ops[0], C);
       auto R1 = findConstantRange(I->Ops[1], C);
       return R0.ashr(R1);
     }
-    case souper::Inst::LShr: {
+    case Inst::LShr: {
       auto R0 = findConstantRange(I->Ops[0], C);
       auto R1 = findConstantRange(I->Ops[1], C);
       return R0.lshr(R1);
     }
-    case souper::Inst::UDiv: {
+    case Inst::UDiv: {
       auto R0 = findConstantRange(I->Ops[0], C);
       auto R1 = findConstantRange(I->Ops[1], C);
       return R0.udiv(R1);
     }
-//     case souper::Inst::SDiv: {
-//       auto R0 = FindConstantRange(I->Ops[0], C);
-//       auto R1 = FindConstantRange(I->Ops[1], C);
-//       return R0.sdiv(R1); // unimplemented
-//     }
-    // TODO: Xor pattern for not, truncs and extends, etc
+      //     case Inst::SDiv: {
+      //       auto R0 = FindConstantRange(I->Ops[0], C);
+      //       auto R1 = FindConstantRange(I->Ops[1], C);
+      //       return R0.sdiv(R1); // unimplemented
+      //     }
+      // TODO: Xor pattern for not, truncs and extends, etc
     default:
       return result;
+    }
   }
-}
 
 }
