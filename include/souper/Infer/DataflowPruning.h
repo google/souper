1766f664bfd544bf22fce398dc6e8b50d5452b46
Manasij Mukherjee <manasij7479@gmail.com>
2019-03-01 21:21:02 -0700

    Do not call pruning code when pruning is disabled

--- include/souper/Infer/DataflowPruning.h
+++ include/souper/Infer/DataflowPruning.h
@@ -9,6 +9,7 @@ namespace souper {
 
 class ValueAnalysis {
 public:
+  ValueAnalysis() {}
   ValueAnalysis(Inst *LHS_, std::vector<ValueCache> Inputs_)
     : LHS(LHS_),Inputs(Inputs_) {
     for (auto &&Input : Inputs) {
@@ -28,18 +29,24 @@ typedef std::function<bool(Inst *, std::vector<Inst *> &)> PruneFunc;
 
 class DataflowPruningManager {
 public:
-  DataflowPruningManager(Inst *LHS, std::vector<Inst *> &Inputs,
+  DataflowPruningManager(Inst *LHS_, std::vector<Inst *> &Inputs_,
                          unsigned StatsLevel);
   PruneFunc getPruneFunc() {return DataflowPrune;}
   void printStats(llvm::raw_ostream &out) {
     out << "Dataflow Pruned " << NumPruned << "/" << TotalGuesses << "\n";
   }
+  void init();
+  // double init antipattern, required because init should
+  // not be called when pruning is disabled
 private:
   ValueAnalysis VA;
   PruneFunc DataflowPrune;
   unsigned NumPruned;
   unsigned TotalGuesses;
+  int StatsLevel;
+  Inst *LHS;
 
+  std::vector<Inst *> &Inputs;
   std::vector<ValueCache> generateInputSets(std::vector<Inst *> &Inputs);
 };
 
