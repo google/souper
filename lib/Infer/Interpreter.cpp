5ea9f774c5138f1651bf5c5bb6a96592861269a7
Zhengyang Liu <liuz@cs.utah.edu>
2019-10-09 04:51:37 +0000

    fix

--- lib/Infer/Interpreter.cpp
+++ lib/Infer/Interpreter.cpp
@@ -112,7 +112,7 @@ namespace souper {
         return EvalValue::ub();
 
     // phi and select only take poison from their chosen input
-    if (Inst->K != Inst::Select) {
+    if (Inst->K != Inst::Select && Inst->K != Inst::Phi) {
       for (auto &A : Args)
         if (A.K == EvalValue::ValueKind::Poison)
           return EvalValue::poison();
