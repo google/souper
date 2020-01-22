888622be34c1dc23962b6d83e23acd6ba3bf8e25
Pranav Kant <pranavk@cs.utah.edu>
2020-01-22 15:54:32 -0700

    test for synthesizing freeze + some modifs in enumerative algorithm

--- lib/Infer/EnumerativeSynthesis.cpp
+++ lib/Infer/EnumerativeSynthesis.cpp
@@ -206,6 +206,10 @@ bool getGuesses(const std::vector<Inst *> &Inputs,
     unaryExclList.push_back(Inst::BitReverse);
   }
 
+  // disable generating freeze of freeze
+  if (unaryHoleUsers.size() == 1 && unaryHoleUsers[0]->K == Inst::Freeze)
+    unaryExclList.push_back(Inst::Freeze);
+
   std::vector<Inst *> PartialGuesses;
 
   std::vector<Inst *> Comps(Inputs.begin(), Inputs.end());
@@ -222,32 +226,33 @@ bool getGuesses(const std::vector<Inst *> &Inputs,
   Comps.push_back(I1);
 
   // Unary Operators
-  if (Width > 1) {
-    for (auto K : UnaryOperators) {
-      for (auto Comp : Comps) {
-        if (std::find(unaryExclList.begin(), unaryExclList.end(), K) != unaryExclList.end())
-          continue;
+  for (auto K : UnaryOperators) {
+    if (std::find(unaryExclList.begin(), unaryExclList.end(), K) != unaryExclList.end())
+      continue;
 
-        if (K == Inst::BSwap && Width % 16 != 0)
-          continue;
+    if (K != Inst::Freeze && Width <= 1)
+      continue;
 
-        if (Comp->K == Inst::ReservedInst) {
-          auto V = IC.createHole(Width);
-          auto N = IC.getInst(K, Width, { V });
-          addGuess(N, Width, IC, LHSCost, PartialGuesses, TooExpensive);
-          continue;
-        }
+    for (auto Comp : Comps) {
+      if (K == Inst::BSwap && Width % 16 != 0)
+        continue;
 
-        if (Comp->Width != Width)
-          continue;
-
-        // Prune: unary operation on constant
-        if (Comp->K == Inst::ReservedConst)
-          continue;
-
-        auto N = IC.getInst(K, Width, { Comp });
+      if (Comp->K == Inst::ReservedInst) {
+        auto V = IC.createHole(Width);
+        auto N = IC.getInst(K, Width, { V });
         addGuess(N, Width, IC, LHSCost, PartialGuesses, TooExpensive);
+        continue;
       }
+
+      if (Comp->Width != Width)
+        continue;
+
+      // Prune: unary operation on constant
+      if (Comp->K == Inst::ReservedConst)
+        continue;
+
+      auto N = IC.getInst(K, Width, { Comp });
+      addGuess(N, Width, IC, LHSCost, PartialGuesses, TooExpensive);
     }
   }
 
