807261227c69687df6b0b8b50b0d1fdb8f35b908
John Regehr <regehr@cs.utah.edu>
2020-04-16 08:49:35 -0600

    we now build a slightly hacked LLVM that optionally disables several peepholes passes. this commit adds tests that that works properly.

---
+++ test/LLVM/all1.c
@@ -0,0 +1,12 @@
+// REQUIRES: solver
+
+// RUN: %clang -O2 -S -o - %s -emit-llvm | %FileCheck -check-prefix=TEST1 %s
+// TEST1: %or = or i32 %b, %a
+
+// RUN: %clang -O2 -S -o - %s -emit-llvm -mllvm -disable-all-peepholes | %FileCheck -check-prefix=TEST2 %s
+// TEST2: %xor = xor i32 %a, %b
+// TEST2-NEXT: %or = or i32 %xor, %a
+
+unsigned foo(unsigned a, unsigned b) {
+  return (a ^ b) | a;
+}
---
+++ test/LLVM/all2.c
@@ -0,0 +1,12 @@
+// REQUIRES: solver
+
+// RUN: %clang -O2 -S -o - %s -emit-llvm | %FileCheck -check-prefix=TEST1 %s
+// TEST1: and i64 %x, 1
+
+// RUN: %clang -O2 -S -o - %s -emit-llvm -mllvm -disable-all-peepholes | %FileCheck -check-prefix=TEST2 %s
+// TEST2: shl i64 %x, 63
+// TEST2-NEXT: lshr i64 %shl, 63
+
+unsigned long foo(unsigned long x) {
+  return ((x << 63) >> 63) + 1;
+}
