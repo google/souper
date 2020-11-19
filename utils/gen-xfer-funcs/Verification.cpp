eb7cb001d2031c1cc96eabc0f43fc67925b20690
Manasij Mukherjee <manasij7479@gmail.com>
2020-11-19 02:12:49 -0700

    Constant range verification

--- utils/gen-xfer-funcs/Verification.cpp
+++ utils/gen-xfer-funcs/Verification.cpp
@@ -3,7 +3,7 @@
 
 namespace souper {
 using namespace z3;
-bool AbstractDomain::Verify(std::string OpName, size_t Width) {
+bool AbstractDomain::Verify(std::string OpName, size_t ConcreteWidth, size_t AbstractWidth) {
   solver s(ctx);
 
   params p(ctx);
@@ -11,17 +11,17 @@ bool AbstractDomain::Verify(std::string OpName, size_t Width) {
 //   p.set("bv_literals", false);
   s.set(p);
   
-  auto concretex = ctx.bv_const("cx", Width);
-  auto concretey = ctx.bv_const("cy", Width);
+  auto concretex = ctx.bv_const("cx", ConcreteWidth);
+  auto concretey = ctx.bv_const("cy", ConcreteWidth);
 
   std::vector<expr> abstract_inputs;
   for (int i =0; i < NumComponents; ++i) {
-    expr abstractx = ctx.bv_const(("ax_"+std::to_string(i)).c_str(), Width);
-    expr abstracty = ctx.bv_const(("ay_"+std::to_string(i)).c_str(), Width);
+    expr abstractx = ctx.bv_const(("ax_"+std::to_string(i)).c_str(), AbstractWidth);
+    expr abstracty = ctx.bv_const(("ay_"+std::to_string(i)).c_str(), AbstractWidth);
 
-    s.add(ComposedMembershipTest(concretex, abstractx, Width, i)); // cx \in \gamma(ax)
+    s.add(ComposedMembershipTest(concretex, abstractx, ConcreteWidth, i)); // cx \in \gamma(ax)
 
-    s.add(ComposedMembershipTest(concretey, abstracty, Width, i)); // cy \in \gamma(ay)
+    s.add(ComposedMembershipTest(concretey, abstracty, ConcreteWidth, i)); // cy \in \gamma(ay)
 
     abstract_inputs.push_back(abstractx);
     abstract_inputs.push_back(abstracty);
@@ -32,7 +32,7 @@ bool AbstractDomain::Verify(std::string OpName, size_t Width) {
   auto abstract_result = Ops[OpName]->ApplyAbstract(abstract_inputs);
 
   s.add(!ComposedMembershipTest(
-    concrete_result, abstract_result, Width, /*idx*/0));
+    concrete_result, abstract_result, ConcreteWidth, /*idx*/0));
   // cr \notin \gamma(ar)
   
   // Does there exist (cx, cy, ax, ay) such that the 
--- utils/gen-xfer-funcs/demo-main.cpp
+++ utils/gen-xfer-funcs/demo-main.cpp
@@ -5,10 +5,10 @@ using namespace souper;
 
 int main(int argc, char **argv) {
   context ctx;
-  auto K0 = std::make_shared<KnownZeroDomain>(ctx);
-  auto K1 = std::make_shared<KnownOneDomain>(ctx);
-
-  ComposedDomain KnownZero({K0, K1}, ctx);
+//   auto K0 = std::make_shared<KnownZeroDomain>(ctx);
+//   auto K1 = std::make_shared<KnownOneDomain>(ctx);
+//
+//   ComposedDomain KnownZero({K0, K1}, ctx);
 
 //   KnownZero.Ops["and"] = std::make_unique<BinOp>(
 //     [](auto a, auto b){return a & b;}, // concrete
@@ -21,23 +21,43 @@ int main(int argc, char **argv) {
 //   );
 //
 //   for (int i = 1; i <= 64; ++i) {
-//     KnownZero.Verify("and", i);
+//     KnownZero.Verify("and", i, i);
 //     std::cout << "\n";
 //   }
 
-    KnownZero.Ops["xor"] = std::make_unique<BinOp>(
-    [](auto a, auto b){return a ^ b;}, // concrete
-    [](std::vector<expr> a)
-    {return (a[0] & a[1]) | (a[2] & a[3]);}
-    // abstract
-    // a[0] = x.KnownZero
-    // a[1] = y.KnownZero
-    // a[3] = x.KnownOne
-    // a[4] = y.KnownOne
+//     KnownZero.Ops["xor"] = std::make_unique<BinOp>(
+//     [](auto a, auto b){return a ^ b;}, // concrete
+//     [](std::vector<expr> a)
+//     {return (a[0] & a[1]) | (a[2] & a[3]);}
+//     // abstract
+//     // a[0] = x.KnownZero
+//     // a[1] = y.KnownZero
+//     // a[3] = x.KnownOne
+//     // a[4] = y.KnownOne
+//   );
+//
+//   for (int i = 1; i <= 64; ++i) {
+//     KnownZero.Verify("xor", i, i);
+//     std::cout << "\n";
+//   }
+
+
+  ConstantRangeDomain CR(ctx);
+
+  for (int i = 2; i <= 32; ++i) {
+      CR.Ops["add"] = std::make_unique<BinOp>(
+    [](auto a, auto b){return a + b;}, // concrete
+#define L(x) a[x].extract(i-1, 0)
+#define H(x) a[x].extract(2*i-1, i)
+
+    [i](std::vector<expr> a) {
+      return concat(L(0) + H(0), L(1) + H(1));
+    }
+#undef L
+#undef H
   );
 
-  for (int i = 1; i <= 64; ++i) {
-    KnownZero.Verify("xor", i);
+    CR.Verify("add", i, 2*i);
     std::cout << "\n";
   }
 
