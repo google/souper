d474921055d71b36c7d72dc4bba9320f9cc412b1
Zhengyang Liu <liuz@cs.utah.edu>
2021-05-04 16:24:59 -0600

    cleanup dockerfile

--- build_docker.sh
+++ build_docker.sh
@@ -2,7 +2,7 @@
 
 # docker system prune -a
 
-tar cz Dockerfile build_deps.sh clone_and_test.sh CMakeLists.txt docs include lib patches runtime scripts test tools utils unittests | docker build -t souperweb -
+tar cz Dockerfile build_deps.sh clone_and_test.sh CMakeLists.txt docs include lib runtime test tools utils unittests | docker build -t souperweb -
 container=$(/usr/bin/docker run -d souperweb true)
 docker export $container | docker import - souperweb_squashed
 docker build -t souperweb_final - < Dockerfile.metadata
