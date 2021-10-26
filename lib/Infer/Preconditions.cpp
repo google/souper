ef23122d52e8ded390b2711122ba18ba765c4b3c
Manasij Mukherjee <manasij7479@gmail.com>
2021-10-26 15:22:19 -0600

    foo

--- lib/Infer/Preconditions.cpp
+++ lib/Infer/Preconditions.cpp
@@ -37,6 +37,136 @@ inferAbstractPreconditions(SynthesisContext &SC, Inst *RHS,
 std::vector<std::map<Inst *, llvm::ConstantRange>>
   inferAbstractCRPreconditions(SynthesisContext &SC, Inst *RHS,
                                Solver *S, bool &FoundWeakest) {
+
+  InstMapping Mapping(SC.LHS, RHS);
+  bool Valid;
+  if (DebugLevel >= 3) {
+    PrintReplacement(llvm::outs(), SC.BPCs, SC.PCs, Mapping);
+  }
+  std::vector<std::pair<Inst *, APInt>> Models;
+
+  if (std::error_code EC = S->isValid(SC.IC, SC.BPCs, SC.PCs, Mapping, Valid, &Models)) {
+    llvm::errs() << EC.message() << '\n';
+  }
+  std::vector<InstMapping> PCCopy = SC.PCs;
+  if (Valid) {
+    FoundWeakest = true;
+    if (DebugLevel > 1) {
+      llvm::errs() << "Already valid.\n";
+    }
+    return {};
+  }
+
+  std::vector<Inst *> Vars;
+  findVars(Mapping.LHS, Vars);
+  std::set<Inst *> FilteredVars;
+
+  for (auto Var : Vars) {
+    std::string NamePrefix = Var->Name;
+    NamePrefix.resize(4);
+    if (!FixItNoVar || Var->K != Inst::Var || NamePrefix == "fake") {
+      FilteredVars.insert(Var);
+    }
+  }
+
+  // std::map<Inst *, llvm::ConstantRange> OriginalState;
+
+  // for (auto V : Vars) {
+  //   OriginalState[V] = V->Range;
+  // }
+
+  std::vector<std::map<Inst *, llvm::ConstantRange>> Results;
+  Inst *Precondition = SC.IC.getConst(llvm::APInt(1, true));
+
+  std::vector<std::map<Inst *, llvm::APInt>> ValidInputs;
+  // unlike for known bits, this is not guaranteed to terminate quickly.
+  size_t Iterations = 0;
+  while (true) {
+    if (Iterations >= 10000) {
+      break;
+      // TODO adjust this threshold
+    } else {
+      Iterations++;
+    }
+
+    std::vector<Inst *> ModelInsts;
+    std::vector<llvm::APInt> ModelVals;
+
+    if (!ValidInputs.empty()) {
+      auto LastInputSet = ValidInputs.back();
+      // for (auto V : Vars) {
+      //   V->KnownOnes = OriginalState[V].OriginalOne;
+      //   V->KnownZeros = OriginalState[V].OriginalZero;
+      // }
+      Inst *NewPre = nullptr;
+      for (size_t i = 0; i < Vars.size(); ++i) {
+        auto &I = Vars[i];
+        auto W = I->Width;
+//          auto Zero = SC.IC.getConst(llvm::APInt(W, 0));
+
+        auto VarConstraint = SC.IC.getInst(Inst::Ne, 1, {I, SC.IC.getConst(LastInputSet[I])});
+
+        if (NewPre) {
+          NewPre = SC.IC.getInst(Inst::Or, 1, {NewPre, VarConstraint});
+        } else {
+          NewPre = VarConstraint;
+        }
+
+      }
+
+      // Do not find an input belonging to a derived abstract set.
+      if (NewPre) {
+        Precondition = SC.IC.getInst(Inst::And, 1, {Precondition, NewPre});
+      }
+    }
+
+    // Find one input for which the given transformation is valid
+    Models.clear();
+    std::string Query = BuildQuery(SC.IC, SC.BPCs, PCCopy, Mapping,
+                                   &ModelInsts, Precondition, true);
+
+
+    S->isSatisfiable(Query, FoundWeakest, ModelInsts.size(),
+                                       &ModelVals, SC.Timeout);
+
+    std::map<Inst *, llvm::APInt> CurrentCE;
+    if (FoundWeakest) {
+      for (unsigned J = 0; J < ModelInsts.size(); ++J) {
+        if (FilteredVars.find(ModelInsts[J]) != FilteredVars.end()) {
+          CurrentCE[ModelInsts[J]] = ModelVals[J];
+        } else {
+          auto Zero = llvm::APInt(ModelInsts[J]->Width, 0);
+          CurrentCE[ModelInsts[J]] = Zero;
+        }
+
+        if (DebugLevel >= 3) {
+          llvm::outs() << "Starting with : " << ModelVals[J] << "\n";
+        }
+      }
+      ValidInputs.push_back(CurrentCE);
+    } else {
+      if (ValidInputs.empty()) {
+        if (DebugLevel >= 3) {
+          llvm::outs() << "Transformation is not valid for any input.\n";
+        }
+        return {};
+      } else {
+        FoundWeakest = true;
+        if (DebugLevel >= 3) {
+          llvm::outs() << "Exhausted search space.\n";
+        }
+        break;
+      }
+    }
+
+    // Widen CurrentCE into the largest possible CR which maintains validity
+    // How to do this??
+
+
+
+
+  }
+
   return {};
 }
 
@@ -89,7 +219,7 @@ std::vector<std::map<Inst *, llvm::KnownBits>>
     std::vector<std::map<Inst *, llvm::KnownBits>> Results;
     Inst *Precondition = SC.IC.getConst(llvm::APInt(1, true));
 
-    while (true) { // guaranteed to terminate
+    while (true) { // guaranteed to terminate in O(Width)
       if (!Results.empty()) {
         bool foundNonTop = false;;
         for (auto R : Results) {
